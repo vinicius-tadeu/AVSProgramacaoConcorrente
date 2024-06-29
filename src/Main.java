91702c2bc7bb8583d743be87f5a027b95ae87ad0
vinicius-tadeu <viniciustadeusores@gmail.com>
2024-06-29 15:23:20 -0300

    Adicionada a lógica para separar as temperaturas por dia e adicionada a função de temperatura mínima

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Main {
+
+    public static double tempMinima(JSONArray tempsDiaria){
+        double minimo = Double.MAX_VALUE;
+
+        for (int j = 0; j < tempsDiaria.length(); j++) {
+            double temperatura = tempsDiaria.getDouble(j);
+            minimo = Math.min(minimo, temperatura);
+        }
+        double result = minimo;
+        return result;
+    }
+
     public static void main(String[] args) throws InterruptedException, URISyntaxException, IOException {
         String capitais = new String(Files.readAllBytes(Paths.get("src\\Capitais.json")));
         JSONArray jsonArray = new JSONArray(capitais);
@@ -46,14 +56,27 @@ public class Main {
             System.out.println("Longitude: " + longitude);
             System.out.println("Longitude encontrada na api: " + apiResponse.get("longitude"));
             System.out.println("Temperaturas recebidas: " + temperaturas);
+            // Retornou a quantidade de 31 dias * 24 (744) horas por dia, ou seja todas as temperaturas por hora de janeiro, que estão por ordem crescente, portanto basta eu fazer um for e realizar as operações separando por arrays de tamanho 24.
+            System.out.println("Quantidade de temperaturas registradas: " + temperaturas.length());
+            int dia = 1;
+            double resultadoTempMin = 0.0;
+            JSONArray listaTemperaturasPorDia = new JSONArray();
+            for (int l = 0; l<temperaturas.length();l++){
+                double temperatura = temperaturas.getDouble(l);
+                listaTemperaturasPorDia.put(temperatura);
 
-            float minimo = Float.MAX_VALUE;
-
-            for (int j = 0; j < temperaturas.length(); j++) {
-                float temperatura = temperaturas.getFloat(j);
-                minimo = Math.min(minimo, temperatura);
+                    if((l+1)%24 == 0) {
+                        resultadoTempMin = tempMinima(listaTemperaturasPorDia);
+                        System.out.println("Dia :" + dia);
+                        System.out.println("Lista de temperaturas do dia " + dia + ": " + listaTemperaturasPorDia);
+                        System.out.println("Temperatura mínima do dia " + dia + ": " + resultadoTempMin);
+                        System.out.println("\n");
+                        listaTemperaturasPorDia.clear();
+                        dia++;
+                    }
             }
-            System.out.println("Valor mínimo das temperaturas: " + minimo);
+
+            System.out.println("Valor mínimo das temperaturas: " + resultadoTempMin);
 
             System.out.println("\n");
 
